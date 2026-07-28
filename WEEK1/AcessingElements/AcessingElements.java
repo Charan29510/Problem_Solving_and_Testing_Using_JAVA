71e4d11b66af273f71604b598f7a9babd08adbc9
Charan29510 <vtu29510@veltech.edu.in>
2026-07-28 23:51:14 +0530

    Add files via upload

---
+++ WEEK1/AcessingElements/AcessingElements.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class AcessingElements{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<n;i++){
+             a[i]=sc.nextInt();
+
+        }
+System.out.println("The element at the index 3 is "+a[2]);
+
+    }
+
+}
